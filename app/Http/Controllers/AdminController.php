a017eebd24a447d457691437df83a8d8729f582d
Anjali04040 <anjali04042018@gmail.com>
2024-10-26 17:39:23 +0530

    Update product in database through admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -31,4 +31,20 @@ class AdminController extends Controller
         session()->flash('success', 'Category Deleted Successfully.');
         return redirect() ->back();
     }
+
+    public function edit_category($id){
+        $data = Category::find($id);
+        return view('admin.edit_category', compact('data'));
+    }
+
+    public function update_category(Request $request,$id){
+        $data = Category::find($id);
+        $data -> category_name = $request ->category;
+        $data ->save();
+
+        session()->flash('success', 'Category Updated Successfully.');
+
+        return redirect('/view_category');
+
+    }
 }
