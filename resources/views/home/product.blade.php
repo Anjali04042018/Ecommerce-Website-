a738b313df5874c83b713747d9243ce53ff4189a
Anjali04040 <anjali04042018@gmail.com>
2024-10-30 12:00:55 +0530

    Display Products details in another page when you clicked details btn

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -10,7 +10,6 @@
       @foreach($product as $products)
     <div class="col-sm-6 col-md-4 col-lg-3">
         <div class="box">
-            <a href="">
                 <div class="img-box">
                     <img src="products/{{$products->image}}" alt="">
                 </div>
@@ -21,7 +20,11 @@
                         <span>{{$products->price}}</span>
                     </h6>
                 </div>
-            </a>
+
+                <div style="padding:10px;">
+                  <a class="btn btn-danger" href="{{url('product_details',
+                  $products->id)}}">Details</a>
+                </div>
         </div>
     </div>
 @endforeach
