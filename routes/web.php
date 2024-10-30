f0e2a4da914fe9e6a4630fb9dd94bf87c52ab1ab
Anjali04040 <anjali04042018@gmail.com>
2024-10-30 11:24:27 +0530

    Fetch data from data  base and display products in home page

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ use App\Http\Controllers\AdminController;
 //     return view('welcome');
 // });
 
-Route::get('/dashboard', function () {
-    return view('home.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('home.index');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -32,7 +32,10 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-route::get('/',[HomeController::class, 'home']);
+route::get('/',[HomeController::class, 'login_home']);
+route::get('/dashboard',[HomeController::class, 'home'])
+->middleware(['auth', 'verified'])->name('dashboard');;
+
 route::get('admin/dashboard',[HomeController::class,'index']);
 
 route::get('view_category',[AdminController::class,'view_category']);
