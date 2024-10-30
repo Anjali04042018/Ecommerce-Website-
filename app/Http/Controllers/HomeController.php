7ac3cb40dfe3401aab224f89165addfc5c473fb0
Anjali04040 <anjali04042018@gmail.com>
2024-10-30 19:44:56 +0530

    Show product cart data using Foreign key and delete data from cart and display total price also in the cart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,4 +70,25 @@ class HomeController extends Controller
 
         return redirect()->back();
     }
+
+    public function mycart(){
+        if(Auth::id()){
+            $user = Auth:: user();
+            $userid = $user->id;
+            $count = Cart::where('user_id',$userid)->count();
+
+            $cart = Cart::where('user_id', $userid)->get();
+        }
+        return view('home.mycart',compact('count','cart'));
+    }
+
+    public function delete_cart($id){
+
+        $cartItem = Cart::find($id);
+
+        if ($cartItem) {
+        $cartItem->delete();
+        return redirect()->back()->with('success', 'Product removed from cart successfully.');
+        } 
+    }
 }
---
+++ resources/views/home/mycart.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>   
+    @include('home.css')
+
+    <style>
+        .dev_deg{
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            margin:60px;
+        }
+        table{
+            border:2px solid black;
+            text-align:center;
+            width:800px;
+        }
+        th{
+            border:2px solid black;
+            text-align:center;
+            color:white;
+            font:20px;
+            font-weight:bold;
+            background-color:black;
+        }
+        td{
+            border:1px solid skyblue;
+        }
+        .cart_value{
+            text-align:center;
+            margin-bottom:70px;
+            padding:18px;
+        }
+    </style>
+</head>
+
+<body>
+  <div class="hero_area">
+
+   @include('home.header')
+
+   <div class="dev_deg">
+    <table>
+        <tr>
+            <th>Product Title</th>
+            <th>Price</th>
+            <th>Image</th>
+            <th>Remove</th>
+            
+        </tr>
+
+        <?php
+
+        $value= 0;
+        ?>
+        @foreach($cart as $cart)
+        <tr>
+            <td>{{$cart->product->title}}</td>
+            <td>{{$cart->product->price}}</td>
+            <td>
+                <img width="150" src="/products/{{$cart->product->image}}" alt="">
+            </td>
+            <td>
+                <a class="btn btn-danger" href="{{url('delete_cart',$cart->id)}}">Remove</a>
+            </td>
+
+        </tr>
+
+        <?php
+            $value = $value+ $cart->product->price;
+        ?>
+        @endforeach
+    </table>
+</div>
+
+<div class="cart_value">
+    <h3>Total Value of cart is :{{$value}}</h3>
+</div>
+ @include('home.footer')
+</body>
+
+</html>
